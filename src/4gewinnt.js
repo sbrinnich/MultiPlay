79b9a9b68815319991153477b5af8c5f67788c97
Phillip <if16b006@technikum-wien.at>
2016-11-24 14:21:41 +0100

    zurücksetzen des paddigs auf den hilfeseiten

--- src/4gewinnt.js
+++ src/4gewinnt.js
@@ -1,13 +1,13 @@
 //Einbinden der Bilder
 
 var hg = new Image();		
-hg.src="img/TicTacToe.png";
+hg.src="img/4-gewinnt/4-Gewinnt-board.png";
 
-var o = new Image();
-o.src="img/TicTacToe_0.png";
+var ro = new Image();
+ro.src="img/4-gewinnt/4-Gewinnt-red.png";
 
-var x = new Image();
-x.src="img/TicTacToe_1.png";
+var bo = new Image();
+bo.src="img/4-gewinnt/4-Gewinnt-blue.png";
 
 var bl = new Image();
 bl.src="img/Blue.png";
@@ -19,10 +19,15 @@ var dr = new Image();
 dr.src="img/Draw.png";
 // Ein Array für die Felder des Spiels
 
-var fieldpos = new Array(3)
-fieldpos[0] = new Array(0,0,0)
-fieldpos[1] = new Array(0,0,0)
-fieldpos[2] = new Array(0,0,0)
+var fieldpos = new Array(7)
+fieldpos[0] = new Array(0,0,0,0,0,0)
+fieldpos[1] = new Array(0,0,0,0,0,0)
+fieldpos[2] = new Array(0,0,0,0,0,0)
+fieldpos[3] = new Array(0,0,0,0,0,0)
+fieldpos[4] = new Array(0,0,0,0,0,0)
+fieldpos[5] = new Array(0,0,0,0,0,0)
+fieldpos[6] = new Array(0,0,0,0,0,0)
+
 
 // turn steht für den Spielzug und win ist die Gewinnüberprüfung
 
@@ -39,7 +44,7 @@ window.onload =function()
 
 
 	var hg = new Image();
-	hg.src="img/TicTacToe.png";
+	hg.src="img/4-gewinnt/4-Gewinnt-board.png";
 
 
 	// onload wird der Hintergrund gesetzt
@@ -76,24 +81,27 @@ window.onclick= function(getMousePos){
 	var draw=0; // schaut ob jemand gewonnen hat
 	var u; // Variable für die Schleife
 	var j; // Variable für die Schleife
-	var g=500; // bestimmt die Abmessungen des Feldes (500x500)
-	var cX = Math.floor(getMousePos.x/(500/3)); // setzt cX auf einen wert zwischen 0 und 2
-	var cY = Math.floor(getMousePos.y/(500/3)); // setzt cY auf einen wert zwischen 0 und 2
+	var höhe=1024; // bestimmt die
+	var breite=754; // bestimmt die Breite
+	var cX = Math.floor(getMousePos.x/(1024/7)); // setzt cX auf einen wert zwischen 0 und 2
+	var cY = Math.floor(getMousePos.y/(754/6)); // setzt cY auf einen wert zwischen 0 und 2
 
+	console.log(cX);
+	console.log(cY);
 
 	// Wenn noch keiner Gewonnen hat wird dieses if ausgeführt
 	if(win != 1)
 	{
 		if(turn%2==0 && fieldpos[cX][cY]==0)	//Kontrolliert ob der Zug gerade ist dann ist x dran und ob schon etwas in dem Feld ist
 		{
-			c.drawImage(x, (g/3*cX)+(g/15), (g/3*cY)+(g/15));	// Zeichnet das Bild des X in das gewählte Feld
+			c.drawImage(bo, (breite/8*cX)+5, (höhe/9*cY)+23);	// Zeichnet das Bild des X in das gewählte Feld
 			turn= turn+1;
 			fieldpos[cX][cY]=1;  // Sagt über 1 das im Feld ein X steht
 		}
 
 		else if(fieldpos[cX][cY]==0)
 		{
-			c.drawImage(o, (g/3*cX)+(g/15), (g/3*cY)+(g/15));	// Zeichnet das Bild des O in das gewählte Feld
+			c.drawImage(ro, (breite/8*cX)+5, (höhe/9*cY)+23);	// Zeichnet das Bild des O in das gewählte Feld
 			turn= turn+1;
 			fieldpos[cX][cY]=2;	// Sagt über 2 das im Feld ein O steht
 		}
@@ -101,7 +109,7 @@ window.onclick= function(getMousePos){
 	
 
 		//Gewinnüberprüfung
-		for(u=0;u<=2;u++)
+		for(u=0;u<=6;u++)
 		{
 		if(fieldpos[u][0] == 1 && fieldpos[u][1] == 1 && fieldpos[u][2] == 1)
 			{ setTimeout(function() {c.drawImage(bl, 0, 0);}, 2000); win=1; return;}
@@ -115,13 +123,13 @@ window.onclick= function(getMousePos){
 			{ setTimeout(function() {c.drawImage(bl, 0, 0);}, 2000); win=1; return;}
 		if(fieldpos[0][0] == 2 && fieldpos[1][1] == 2 && fieldpos[2][2] == 2 || fieldpos[0][2] == 2 &&  fieldpos[1][1] == 2 && fieldpos[2][0] == 2)
 			{ setTimeout(function() {c.drawImage(red, 0, 0);}, 2000); win=1; return;}
-		for(j=0;j<=2;j++)
+		for(j=0;j<=5;j++)
 			{
 				if(fieldpos[u][j] == 1 || fieldpos[u][j]== 2)
 					{draw++}	
 			}
 			console.log(draw);
-		if(draw == 9)
+		if(draw == 42)
 			{ setTimeout(function() {c.drawImage(dr, 0, 0);}, 2000); win=1;}
 		}
 	}
