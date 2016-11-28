7b005c8d7ed9a8b4451ea459e352a3644f670c84
BuildTools <if16b030@technikum-wien.at>
2016-11-28 13:49:44 +0100

    Added function to call game respectively from index site

--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,44 @@
 // Start 4 gewinnt: load_4gewinnt(canvas);
 
 window.onload = function() {
+    var gametype = findGetParameter("name");
+    if(gametype == "tictactoe"){
+        //TODO ask for name and team
+        play_tictactoe();
+    }else if(gametype == "4gewinnt"){
+        //TODO ask for name and team
+        play_4gewinnt();
+    }
+};
+
+function load_canvas() {
     var canvases = document.getElementsByTagName("canvas");
-    var active_canvas = null;
+    active_canvas = null;
     for(var i = 0; i < canvases.length; i++){
         if(canvases[i].style.display != "none"){
             active_canvas = canvases[i];
             break;
         }
     }
+}
 
-    // TODO ask which game was chosen and load correct one
+function play_tictactoe() {
+    load_canvas();
     load_tictactoe(active_canvas);
-};
+}
+
+function play_4gewinnt() {
+    load_canvas();
+    load_4gewinnt(active_canvas);
+}
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
