f991cc03d1d27967158f3e1d8d8861d6715536e5
BuildTools <if16b030@technikum-wien.at>
2016-12-09 18:55:07 +0100

    Added comments

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,18 @@
-
+// Start game depending on get-parameter 'name' which should contain the game-name
 window.onload = function() {
     var gametype = findGetParameter("name");
     if(gametype == "tictactoe"){
-        //TODO ask for name and team
+        //TODO ask for team
         play_tictactoe();
     }else if(gametype == "4gewinnt"){
-        //TODO ask for name and team
+        //TODO ask for team
         play_4gewinnt();
     }
 };
 
+/**
+ * Load currently active canvas into global variable active_canvas(changes depending on screen-size)
+ */
 function load_canvas() {
     var canvases = $('canvas');
     active_canvas = null;
@@ -21,16 +24,27 @@ function load_canvas() {
     }
 }
 
+/**
+ * Start tic-tac-toe game
+ */
 function play_tictactoe() {
     load_canvas();
     load_tictactoe(active_canvas);
 }
 
+/**
+ * Start 4-gewinnt game
+ */
 function play_4gewinnt() {
     load_canvas();
     load_4gewinnt(active_canvas);
 }
 
+/**
+ * Searches a specified get-parameter in current url and returns its value
+ * @param parameterName name of requested get-parameter
+ * @returns value of requested parameter
+ */
 function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
@@ -43,7 +57,7 @@ function findGetParameter(parameterName) {
 }
 
 /**
- * Retrieve/Save data from/to database
+ * Retrieve/Delete/Save data from/to database
  * @param type either get or insert or delete
  * @param args if type is get: name of requested view in database <br />
  *              if type is insert: array with following information: [name_of_table, [n_values]] <br />
