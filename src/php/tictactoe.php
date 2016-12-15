7dc9c33a741ef155df0f3b876cd2853e431662da
Alexander Dietrich <alexanderdietrich@gmx.at>
2016-12-15 14:06:03 +0100

    Gewinnüberprüfung TicTacToe

--- src/php/tictactoe.php
+++ src/php/tictactoe.php
@@ -45,35 +45,30 @@ function tictactoe_getFieldArray($dbField){
 function tictactoe_checkWinner($field, $last_turn, $teams){
     // TODO implement
     // Should return either team name if one has won or 'draw' or null
-    
+
+    $meinteam = $field[$last_turn['posy']][$last_turn['posx']];
+    $draw = 0;
+
     for($u=0;$u<=2;$u++)
             {
-                if($field[$u][0] == 'blue' && $field[$u][1] == 1 && $field[u][2] == 1)
-                { setTimeout(function() {c.drawImage(bl, 0, 0);}, 2000); win=1; return;}
-                if(fieldpos[0][u] == 1 && fieldpos[1][u] == 1 && fieldpos[2][u] == 1)
-                { setTimeout(function() {c.drawImage(bl, 0, 0);}, 2000); win=1; return;}
-                if(fieldpos[u][0] == 2 && fieldpos[u][1] == 2 && fieldpos[u][2] == 2)
-                { setTimeout(function() {c.drawImage(red, 0, 0);}, 2000); win=1; return;}
-                if(fieldpos[0][u] == 2 && fieldpos[1][u] == 2 && fieldpos[2][u] == 2)
-                { setTimeout(function() {c.drawImage(red, 0, 0);}, 2000); win=1; return;}
-                if(fieldpos[0][0] == 1 && fieldpos[1][1] == 1 && fieldpos[2][2] == 1 || fieldpos[0][2] == 1 &&  fieldpos[1][1] == 1 && fieldpos[2][0] == 1)
-                { setTimeout(function() {c.drawImage(bl, 0, 0);}, 2000); win=1; return;}
-                if(fieldpos[0][0] == 2 && fieldpos[1][1] == 2 && fieldpos[2][2] == 2 || fieldpos[0][2] == 2 &&  fieldpos[1][1] == 2 && fieldpos[2][0] == 2)
-                { setTimeout(function() {c.drawImage(red, 0, 0);}, 2000); win=1; return;}
-                for(j=0;j<=2;j++)
-                {
-                    if(fieldpos[u][j] == 1 || fieldpos[u][j]== 2)
-                    {draw++}
-                }
-                if(draw == 9)
-                { setTimeout(function() {c.drawImage(dr, 0, 0);}, 2000); win=1;}
-            }
-}
+                if($field[$u][0] == $meinteam && $field[$u][1] == $meinteam && $field[$u][2] == $meinteam)
+                { return $meinteam;}
+                if($field[0][$u] == $meinteam && $field[1][$u] == $meinteam && $field[2][$u] == $meinteam)
+                { return $meinteam;}
+                if($field[0][0] == $meinteam && $field[1][1] == $meinteam && $field[2][2] == $meinteam || $field[0][2] == $meinteam &&  $field[1][1] == $meinteam && $field[2][0] == $meinteam)
+                { return $meinteam;}
 
-},false);
-}
+                for($j=0;$j<=2;$j++)
+                {
+                    if($field[$u][$j] != null )
+                    {$draw++;}
+                }
+                if($draw == 9)
+                { return 'draw';}
+            }
     return null;
 }
+
 
 
 ?>
