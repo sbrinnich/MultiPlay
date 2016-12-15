d239895534a1d3b1296d0dc8c5f18e0e45bc6741
Gumi22 <gumi22-_-@hotmail.com>
2016-12-15 18:08:46 +0100

    Added 2nd Aktueller-Stand to git

--- src/php/4gewinnt.php
+++ src/php/4gewinnt.php
@@ -36,6 +36,62 @@ function viergewinnt_randomTurn($field){
 function viergewinnt_checkWinner($field, $last_turn){
     // TODO implement
     // Should return either team name if one has won or 'draw' or null
+
+    $x = $last_turn['posx']; //x-position of last turn
+    $y = $last_turn['posy']; //y-position of last turn
+    $team = $field[$y][$x]; //team that just played and has to be
+    $inarow = 1;
+
+    //topright - bottomleft
+
+
+    //topleft - bootomright
+
+
+    //horizontal
+
+
+    //down
+    if($y <= 2){ //if high enough, check down
+        if($field[$y][$x+1] == $team && $field[$y][$x+2] == $team && $field[$y][$x+3] == $team)
+        {
+            return $team;
+        }
+    }
+
+    //is it a draw?
+    $draw = true;
+    for($x = 0; $x < 7; $x++)//only top row is relevant
+    {
+        if($field[0][$x] == null){ //if field is still empty game is not finished jet
+            $draw = false;
+            break;
+        }
+    }
+    if($draw == true)
+    {
+        return 'draw';
+    }
+
+    //else continue game, no winner was found
+    return null;
+}
+
+function are_4_in_a_row($field, $team, $x, $y, $deltax, $deltay){
+    //return team if win, or null if not
+    $tries = 1;
+    while ($tries <= 4)//try on each position of possible
+    {
+        0
+         0
+          0
+           0
+
+
+        $tries++;
+    }
+
+
     return null;
 }
 
